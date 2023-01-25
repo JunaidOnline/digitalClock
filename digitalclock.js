c59a6651c19c542e7a6640af45bb984311dc399f
Junaid Akhtar <junaidakhtarconnects@gmail.com>
2023-01-25 15:03:11 +0530

    digitalClock js file

---
+++ digitalclock.js
@@ -0,0 +1,16 @@
+function displayTime()
+{
+    let current = new Date();
+
+    let hours = current.getHours();
+    let mins = current.getMinutes();
+    let sec = current.getSeconds();
+
+    document.getElementById("hrs").value = hours;
+    document.getElementById("min").value = mins;
+    document.getElementById("sec").value = sec;
+
+
+}
+
+setInterval(()=>(displayTime()),1000);
